bced9dd54390542b3cc2cb708caddac3148d4751
wkencel <wkencel1@gmail.com>
2020-12-19 20:42:09 -0500

    12/19 8:42 PM

--- server/server.js
+++ server/server.js
@@ -6,6 +6,44 @@ const morgan = require('morgan')
 const marvelSchema = require('./model/marvelModel')
 const routes = require('./routes/api')
 const PORT = 3000;
+// Will code
+// make into a non hard coded token
+const { useState, useCallback, useEffect } = require('react');
+const axios = require('axios');
+const accessToken = 'iuhyegedbeuytvdyevdyt3222u7e2uhwjdhbsd'
+const apiUrl = 'http://localhost:3000/api';
+
+// axios.interceptors.request.use(
+//   config => {
+//     config.headers.authorization = `Bearer ${accessToken}`;
+//     return config;
+//   },
+//   error => {
+//     return Promise.reject(error);
+//   });
+
+const authAxios = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    Authorization: `Bearer ${accessToken}`
+  }
+})
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [requestError, setRequestError] = useState();
+
+  const fetchData = useCallback(async () => {
+    try {
+      // fetch and set users
+      const result = await authAxios.get(`/users/all`)
+    } catch (err) {
+      // set request error message
+      setRequestError(err.message);
+    }
+  });
+
+}
 
 
 app.get('/', (req, res) => {
