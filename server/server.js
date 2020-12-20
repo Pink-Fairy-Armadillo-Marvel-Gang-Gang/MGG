876188dd756bfc40b8f4bfa2ee1af8fd06d6fdb5
wkencel <wkencel1@gmail.com>
2020-12-19 23:56:31 -0500

    11:56 PM

--- server/server.js
+++ server/server.js
@@ -8,49 +8,7 @@ const routes = require('./routes/api')
 const PORT = 3000;
 const App = './components/App.jsx'; // do i have to path this? how do i get the data from this
 // Will code
-// make into a non hard coded token
 
-/*
-
-const { useState, useCallback, useEffect } = require('react');
-const axios = require('axios');
-const accessToken = 'iuhyegedbeuytvdyevdyt3222u7e2uhwjdhbsd'
-const apiUrl = 'http://localhost:3000/api';
-
-const path = require('path');
-// axios.interceptors.request.use(
-//   config => {
-//     config.headers.authorization = `Bearer ${accessToken}`;
-//     return config;
-//   },
-//   error => {
-//     return Promise.reject(error);
-//   });
-
-const authAxios = axios.create({
-  baseURL: apiUrl,
-  headers: {
-    Authorization: `Bearergit  ${accessToken}`
-  }
-})
-
-function App() {
-  const [users, setUsers] = useState([]);
-  const [requestError, setRequestError] = useState();
-
-  const fetchData = useCallback(async () => {
-    try {
-      // fetch and set users
-      const result = await authAxios.get(`/users/all`)
-    } catch (err) {
-      // set request error message
-      setRequestError(err.message);
-    }
-  });
-  // return component here
-}
-
-*/
 
 app.get('/', (req, res) => {
     return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
