cec525201a6a6efe55e251de358d684227c98343
Vlad Munteanu <munteanuv@gmail.com>
2020-12-28 14:00:50 -0500

    database wip

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,27 +1,110 @@
 import React from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useForm } from "react-hook-form";
+
 // import '../style.css';
 
 
 export default function App() {
   // destructure useForm
   const {register, handleSubmit, watch, errors} = useForm();
-  // define onSubmit handler function
+
+    // register form
+    const {
+        register: register2,
+        errors: errors2,
+        handleSubmit: handleSubmit2
+      } = useForm({
+        mode: "onBlur"
+      });
+
+
+//use react hooks
+const [usernameReg, setUsernameReg] = useState('')
+const [passwordReg, setPasswordReg] = useState('')
+
+const [username, setUsername] = useState('')
+const [password, setPassword] = useState('')
+
+const [loginStatus, setLoginStatus] = useState('')
+
+
+  // define onSubmit handler function for login
   const onFormSubmit = (data) => {
-      console.log(data);
+      console.log('login', data);
+      setPassword(data.password);
+      setUsername(data.username);
+      console.log('login', data);
+      // vm: store username and password as environment variables to use when quering database? 
+      process.env.username = data.username;
+      process.env.password = data.password;
+      
   }
+
+  // define onSubmit handler function for registration
+  const onFormSubmit2 = (data) => {
+    console.log('reg',data);
+    setPasswordReg(data.password);
+    setUsernameReg(data.username);
+    console.log('reg',data);
+    // vm: store username and password as environment variables to use when quering database? 
+    process.env.username = data.username;
+    process.env.password = data.password;
+    signup();
+}
+
+
+// axios post request // put this function in the form submit
+//   const signup = () => {
+//       Axios.post("http://localhost/8080/register", {
+//           username: usernameReg,
+//           password: passwordReg,
+//   }).then((res) => {
+//       console.log(response);
+//   }); 
+//   }
+
+const signup = () => {
+    axios.post("/signup", {
+        username: username,
+        password: password,
+}).then((res) => {
+    if (res.data.message) {
+        setLoginStatus(res.data.message)
+    } else {
+        setLoginStatus(res.data[0].username)
+    }
+    console.log(res)
+  }); 
+}
+
+
+// console.log('login status',loginStatus)
   // invoke watch function from useForm destructuring
 
 //   return our html/componenet stuf
 return (
     <div>
+    <h1>LOGIN</h1>
     <form name="signin" onSubmit={handleSubmit(onFormSubmit)}>
         <input type="text" name="username" ref={register} placeholder="Username"></input>
         <input type="password" name="password" ref={register} placeholder="Password"></input>
         <input type="submit"/>
     </form>
+
+    <h3>login status here here{loginStatus}</h3>
+
+   
+
+    <h1>Registration</h1>
+    <form name="signin" onSubmit={handleSubmit2(onFormSubmit2)}>
+        <input type="text" name="username2" ref={register2} placeholder="Username"></input>
+        <input type="password" name="password2" ref={register2} placeholder="Password"></input>
+        <input type="submit"/>
+    </form>
     </div>
 )
   
+};
+
--- server/server.js
+++ server/server.js
@@ -1,23 +1,57 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 // const path = require('path')
 const morgan = require('morgan')
-const marvelSchema = require('./model/marvelModel')
+const db = require('./model/marvelModel')
 const routes = require('./routes/api')
+const { v4: uuidv4 } = require('uuid')
 const PORT = 3000;
-const App = './components/App.jsx'; // do i have to path this? how do i get the data from this
-// Will code
+const App = require('../client/components/App'); // do i have to path this? how do i get the data from this
 
+// VM: WE MIGHT NEED TO NPM INSTALL PG-PROMISE BUT NOT SURE
 
-app.use(express.json()); // check this
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('client'));
 
 
+// app.get('/profile/:name', function(req,res){
+//     res.render('profile', {person: req.params.name})
+// })
+
+
+app.use('/', (req, res) => {
+  db.createFavoritesTable();
+});
+
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 })
+
+app.post('/signup', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log('test at backend')
+  db.query("INSERT INTO users (id, username, password) VALUES ($1,$2, $3)",
+    [uuidv4(), 
+      username, 
+      password],
+    (err, result) => {
+      console.log(err)
+    }
+  );
+});
+
+
+app.get('/:id', (req, res) => {
+  const newFav = req.params;
+  db.query('SELECT * FROM people WHERE people._id = $1', [req.params.id])
+    .then((result) => console.log(result.rows[0]))
+    .catch((err) => console.log(`ERROR: ${err}`));
+});
+
+
+
 
 /*
 
