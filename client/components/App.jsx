aa8a1a3241d951b4244b57f4f3313363620d27d4
Super-Programmer-Navi <navdip.dhillon.91@gmail.com>
2020-12-28 14:26:39 -0800

    new

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,16 +11,13 @@ import HomePage from '../components/homepage';
 
 
 
-class App extends React.Component() {
-
-    constructor() {
-        super();
+export default function App() {
 
   // destructure useForm
-    const {register, handleSubmit, watch, errors} = useForm();
+  const {register, handleSubmit, watch, errors} = useForm();
   // define onSubmit handler function
-    const onFormSubmit = (data) => {
-        console.log(data);
+  const onFormSubmit = (data) => {
+      console.log(data);
       
 
     //   const signin = () => {
@@ -39,36 +36,32 @@ class App extends React.Component() {
     
     // }
   }
-}
 
-    render(){
-    return (
-        <Switch>
 
-        <Route exact path='/'>
-        <div>
-        <form name="signin" onSubmit={this.props.handleSubmit(this.props.onFormSubmit)}>
-            <input type="text" name="username" ref={this.props.register} placeholder="Username"></input>
-            <input type="password" name="password" ref={this.props.register} placeholder="Password"></input>
-            <input type="submit"/>
-        </form>
+return (
+    <Switch>
+
+    <Route exact path='/'>
+    <div>
+    <form name="signin" onSubmit={handleSubmit(onFormSubmit)}>
+        <input type="text" name="username" ref={register} placeholder="Username"></input>
+        <input type="password" name="password" ref={register} placeholder="Password"></input>
+        <input type="submit"/>
+    </form>
           <Link to="/signup"><button>Create an Account!</button></Link>
-        </div>
-        </Route>
-        <Route path='/signup'>
+    </div>
+      </Route>
+      <Route path='/signup'>
                     <div>
                         <SignUp />
                     </div>
-        </Route>
+    </Route>
 
-        <Route path='/homepage'>
+    <Route path='/homepage'>
                     <div>
                         <HomePage />
                     </div>
-        </Route>
-        </Switch>
-    );
-    }   
+    </Route>
+    </Switch>
+)
 };
-
-export default App;
