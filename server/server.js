df9a36df29c8a1716b3bcc737e51259bf81db799
Super-Programmer-Navi <navdip.dhillon.91@gmail.com>
2020-12-27 21:03:43 -0800

    update

--- server/server.js
+++ server/server.js
@@ -14,10 +14,12 @@ app.use(express.static('client'));
 
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 })
 
-
+app.get('/signup', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../client/pages/signup.html'));
+})
 
 
 app.use(morgan('tiny'));
